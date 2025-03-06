73d00f597a8dead5f677e4ed7fc361d27213b74e
Bruno Silvano Rodrigues <brunorodrigues.tec.info@gmail.com>
2025-03-06 01:30:44 +0000

    Testando deploy automatico

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Life is so much simplier when you stop explaining yourself to people and just do what works for you.</h1>;
+    return <body>
+        <h1>Life is so much simplier when you stop explaining yourself to people and just do what works for you.</h1>
+        <h6>___ Unknown</h6>
+        </body>;
 }
 
 export default Home;    
