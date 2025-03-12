cc0a33263fa6c0a794f96e0ed839aa80114a5911
Bruno Silvano Rodrigues <brunorodrigues.tec.info@gmail.com>
2025-03-12 01:12:05 +0000

    adiciona scripts  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone do tab news para treinamento de técnicas e tecnologias de front end
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 function Home() {
-    return <body>
-        <h1>Life is so much simplier when you stop explaining yourself to people and just do what works for you.</h1>
-        <h6>___ Unknown</h6>
-        </body>;
+  return (
+    <body>
+      <h1>
+        Life is so much simplier when you stop explaining yourself to people and
+        just do what works for you.
+      </h1>
+      <h6>___ Unknown</h6>
+    </body>
+  );
 }
 
-export default Home;    
+export default Home;
